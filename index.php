561f16fd10f93ad37de76e01412f3a70b44bbe1a
huang-hua1212 <hahahazzz123@gmail.com>
2022-12-24 13:08:12 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- <!DOCTYPE html> -->
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <script
+  src="https://code.jquery.com/jquery-2.2.4.min.js"
+  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+  crossorigin="anonymous"></script>
+  <title>
+  </title>
+</head>
+<body>
+<div id='app'></div>
+</body>
+<script type = 'module'>
+  import router from '/router/index.js';
+
+</script>
+</html>
---
+++ router/index.js
@@ -0,0 +1,63 @@
+import Router from '/lib/router/index.js';
+
+
+const routes = [
+  {
+    path: '/',
+    callback: function() {
+      console.log('main page');
+      console.log(self.routerInstance);
+      // document.getElementById('app').innerHTML = `<div>Main Page~~~~~~~</div>`;
+      $("#app").load("/views/mainPage.html"); 
+    },
+    name: 'main-page'
+  },
+  {
+    path: '/:id',
+    callback: function(id) {
+      self.routerInstance.params = id;
+      
+      console.log('參數為:', id);
+      console.log(self.routerInstance);
+
+    },
+    name: 'aa'
+  },
+  {
+    group: 'group1',
+    path: 'show',
+    callback: function() {
+      console.log('group1/show');
+
+    },
+    name: 'cc'
+  },
+  {
+    path: '/post/3/show',
+    callback: function() {
+      console.log('aa page')
+    },
+    name: 'cc'
+  }
+]
+const router = new Router({mode: 'history', routes});
+router.add('/add-new-uri/test', function () {
+    console.log(`This is the post add-new-uri`)
+}, 'add-new-uri')
+router.group('test', function () {
+  router.add('api/:id', function (id) {
+      console.log("Route prefixed", id)
+  })
+
+  router.group('sub', function () {
+      router.add('api/:id', function (id) {
+          console.log("Route sub prefixed", id)
+      })
+  })
+  router.add('aze', function () {
+      console.log("Route aze")
+  })
+})
+
+self.routerInstance = router; 
+export default router;
